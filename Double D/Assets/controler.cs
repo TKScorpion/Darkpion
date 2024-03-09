9ee55ddff4facc6a4b9056dd63d6e73f615c1827
Potat05Sensei <rttr900r@gmail.com>
2024-03-09 22:56:37 +0300

    m

--- Double D/Assets/controler.cs
+++ Double D/Assets/controler.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class controler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private float horizontal;
+    private float speed = 10f;
+    private float jumpingPower = 18f;
+    private bool isFacingRight = true;
+
+    [SerializeFeild] private Rigidbody2D rb;
+    [SerializeFeild] private Transform groundcheck; 
+    [SerializeFeild] private LayerMask groundLayer;
+
+
+     
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
         
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5);
+        }
+
+        Flip();
+
     }
+
+    private bool IsGrounded()
+    {
+        return Physiscs2D.OverlapCircle(groundcheck.position, 0.2f, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    
 }
